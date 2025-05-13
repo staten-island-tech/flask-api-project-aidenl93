0e7644ae998e15e4f783b20efa09935b37b23845
aidenl93 <aidenl93@nycstudents.net>
2025-05-13 10:02:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    response = requests.get('https://api.fbi.gov/wanted/v1/list')
+    response = requests.get('https://api.fbi.gov/wanted/v1/list', params={
+    'title': 'Cyber Crimes',
+    'page': 1,
+})
     data = response.json()
     return render_template('index.html', items=data['items'])
 def wanted():
-    response = requests.get('https://api.fbi.gov/wanted/v1/list')
+    response = requests.get('https://api.fbi.gov/wanted/v1/list', params={
+    'title': 'Cyber Crimes',
+    'page': 1,
+
+})
     data = response.json()
     return render_template('wanted.html', items=data['items'])
 
