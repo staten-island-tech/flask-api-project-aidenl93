a6cb2c79e2ef0be540b51030fbcc0e2fe916ea96
aidenl93 <aidenl93@nycstudents.net>
2025-05-14 09:59:07 -0400

    we

--- app.py
+++ app.py
@@ -11,15 +11,21 @@ def index():
 })
     data = response.json()
     return render_template('index.html', items=data['items'])
-def wanted():
+@app.route("/wanted/<uid>")
+def wanted(uid):
     response = requests.get('https://api.fbi.gov/wanted/v1/list', params={
     'title': 'Cyber Crimes',
     'page': 1,
 
 })
     data = response.json()
-    return render_template('wanted.html', items=data['items'])
 
+    # Find the person by uid
+    for item in data['items']:
+        if item['uid'] == uid:
+            return render_template('wanted.html', item=item)
+
+    return "Not Found", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
