f91f9a495eeec86a71cd5142d13242fb4feb7c93
aidenl93 <aidenl93@nycstudents.net>
2025-05-20 09:31:54 -0400

    we

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    page = request.args.get('page', 1, type=int)
     response = requests.get('https://api.fbi.gov/wanted/v1/list', params={
-    "description": "Conspiracy to Commit Computer Hacking"
-    
+    'race': 'asian',
+    'page': page,
+    'pagesize': 200
 })
     data = response.json()
-    return render_template('index.html', items=data['items'])
+    return render_template('index.html', items=data['items'], page=page, total=data['total'])
 @app.route("/wanted/<uid>")
 def wanted(uid):
     response = requests.get('https://api.fbi.gov/wanted/v1/list', params={
-    "description": "Conspiracy to Commit Computer Hacking"
+    'race': 'asian'
 })
     data = response.json()
-
     # Find the person by uid
     for item in data['items']:
         if item['uid'] == uid:
